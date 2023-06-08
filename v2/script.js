21a88e27fbf2bbaca47f3542926032a25f9ff5f4
Mohammed Ruknuddin <iosrukn123@gmail.com>
2023-06-08 11:33:05 +0300

    Portfolio 2.0

--- v2/script.js
+++
@@ -1,9 +0,0 @@
-// JavaScript code to make the navbar responsive and full-screen
-const toggleButton = document.getElementById("toggleButton");
-const navLinks = document.querySelector(".nav-links");
-const primaryNavigation = document.querySelector(".primary-navigation");
-
-toggleButton.addEventListener("click", () => {
-  navLinks.classList.toggle("show");
-  primaryNavigation.classList.toggle("full-screen");
-});
